0ea3e3a14bee64caeed993a1c30c1af22521f825
Charlie Truong <65761393+daseann@users.noreply.github.com>
2023-10-18 19:05:09 -0700

    Create Fibonacci.java

---
+++ Java/Fibonacci.java
@@ -0,0 +1,13 @@
+// Looping: Write a Java program that prints the first 10 numbers in the Fibonacci sequence.
+package Java;
+
+class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10, num1 = 0, num2 = 0;
+        for (int i = 0; i < n; i++)
+        System.out.print(" " + num1);
+
+
+    }
+
+}
